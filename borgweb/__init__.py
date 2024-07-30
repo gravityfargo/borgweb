4f912cafd4d46deb6cc1c1848b438da5eaddff9b
Nathan Price <gravityfargo@gmail.com>
2024-07-29 22:50:21 -0400

    added flask boiler

--- borgweb/__init__.py
+++ borgweb/__init__.py
@@ -1,1 +1,32 @@
-from ._version import version as __version__
+# from ._version import version as __version__
+import os
+from flask import Flask
+from . import db
+from .views import blueprint
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "borgweb.sqlite"),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    db.init_app(app)
+    
+    from .views import auth
+    app.register_blueprint(blueprint)
+
+    return app
--- borgweb/wsgi.py
+++ borgweb/wsgi.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from .app import create_app
+from . import create_app
 
 if __name__ == "__main__":
     create_app.run()
