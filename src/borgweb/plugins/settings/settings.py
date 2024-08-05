b52ba27438ec791b889ab0b42784b4ec4ce07576
Nathan Price <gravityfargo@gmail.com>
2024-08-04 20:38:27 -0400

    adding a default config

--- src/borgweb/plugins/settings/settings.py
+++ src/borgweb/plugins/settings/settings.py
@@ -1,13 +1,19 @@
-""" plugins/settings/settings.py """
-
 from borgweb.plugins import Plugin
+from borgweb.settings import Config
 from flask import render_template
 
-class Settings(Plugin):
+class Settings(Plugin, Config):
     backend_only = False
     plugin_name = "settings"
     bootstrap_icon = "bi-gear"
     nav_display_name = "Settings"
+
+    default_config = {
+        "defaults": {
+            "archive_name": "archive-{hostname}-{now}"
+            "create_backup_command": "borg create --list REPO_DIRECTORY::ARCHIVE_NAME ."
+        }
+    }
 
     def setup_routes(self):
         @self.blueprint.route("/")
